3d51ced41c4e151e2b57e4307a0203a3bbb48a12
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-10-15 14:03:27 +0200

    Prueba4 fin fin

--- Tema2.Practica4/src/main/java/ListaPokemons.java
+++ Tema2.Practica4/src/main/java/ListaPokemons.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,9 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+
 public class ListaPokemons {
-        private int count;
-        private String next;
-        private String previous;
         private List<Pokemon> results;
 
 }
--- Tema2.Practica4/src/main/java/SolicitudDatosApi.java
+++ Tema2.Practica4/src/main/java/SolicitudDatosApi.java
@@ -1,5 +1,7 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -11,7 +13,13 @@ public class SolicitudDatosApi {
     private static final String BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 
     // Metodo para hacer la solicitud GET a la API
-    private String getApiData(String urlString) throws Exception {
+    private String getApiData() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+        JsonNode rootNode = objectMapper.readTree(new URL("https://pokeapi.co/api/v2/pokemon/"));
+        return  rootNode.toString();
+        //List<Location> locations = objectMapper.readValue(rootNode.get("results").traverse(), new TypeReference<>(){});
+    /*
         URL url = new URL(urlString);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -30,6 +38,8 @@ public class SolicitudDatosApi {
 
         conn.disconnect();
         return response.toString();
+
+     */
     }
 
     // Metodo para obtener los primeros 20 Pokémon (sin detalles adicionales)
@@ -37,7 +47,7 @@ public class SolicitudDatosApi {
         ObjectMapper objectMapper = new ObjectMapper();
 
         // Hacemos la solicitud a la API para obtener la lista de los primeros 20 Pokémon
-        String jsonResponse = getApiData(BASE_URL);
+        String jsonResponse = getApiData();
 
         // Deserializamos la respuesta de la lista de Pokémon
         ListaPokemons pokemonListResponse = objectMapper.readValue(jsonResponse, ListaPokemons.class);
@@ -51,7 +61,8 @@ public class SolicitudDatosApi {
         String url = "https://pokeapi.co/api/v2/pokemon/" + identifier;  // Construimos la URL para el Pokémon
 
         // Hacemos la solicitud a la API para obtener los detalles del Pokémon
-        String jsonResponse = getApiData(url);
+        JsonNode rootNode = objectMapper.readTree(new URL(url));
+        String jsonResponse = rootNode.toString();
 
         // Deserializamos el JSON del Pokémon en un objeto Pokémon
         return objectMapper.readValue(jsonResponse, PokemonDetallado.class);
